154dce8fb5a1cfc79fb117817625e82b3793b956
Addison Verger <addison.j.verger@gmail.com>
2019-02-10 21:35:07 -0600

    form rendering

--- form/app.js
+++ form/app.js
@@ -1,17 +1,39 @@
-let TextInput = () => {
-    return <span>Make this component render a styled text input tag</span>
+let TextInput = (props) => {
+    return <div style={{padding: "5px 0"}}>
+      <div>{props.label}</div>
+      <input type={props.type} name={props.name} style={{width: "25%"}}/>
+    </div>
 }
 
-let YesNoRadio = () => {
-    return <span>Make this component render two styled radio button side by side labelled "yes" and "no"</span>
+let YesNoRadio = (props) => {
+    return <div style={{padding: "5px 0"}}>
+      <div>{props.label}</div>
+      <span style={{padding: "0 5px"}}>
+        <input type="radio" name={props.name} value="yes" /> Yes
+      </span>
+      <span style={{padding: "0 5px"}}>
+        <input type="radio" name={props.name} value="no" /> No
+      </span>
+    </div>
 }
 
 let SubmitButton = () => {
-    return <span>Make this component render a styled button of type "submit"</span>
+    return <div style={{padding: "5px 0"}}>
+      <input type="submit" value="Submit" style={{background: "#e7e7e7", borderRadius: "4px", padding: "10px 24px"}} />
+    </div>
 }
 
 let Form = () => {
-    return <span>Make this Component render a form tag with 3 text inputs, two yes/no radio buttons, and a submit button </span>
+    return <form>
+      <div>Make this Component render a form tag with 3 text inputs, two yes/no radio buttons, and a submit button</div>
+      <h1>My Form</h1>
+      <TextInput label="First Name:" type="text" name="fname"/>
+      <TextInput label="Last Name:" type="text" name="lname"/>
+      <TextInput label="Email:" type="email" name="email"/>
+      <YesNoRadio label="Would you like to sign-up for our Newsletter?" name="newsletter"/>
+      <YesNoRadio label="Another question?" name="question"/>
+      <SubmitButton />
+    </form>
 }
 
 
